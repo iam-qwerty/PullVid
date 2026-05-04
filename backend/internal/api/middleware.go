dea87c264467aa5a84354765a1d8a4d3bb19b837
iam-qwerty <eabiodun45@gmail.com>
2026-05-04 05:27:12 +0100

    update

--- backend/internal/api/middleware.go
+++ backend/internal/api/middleware.go
@@ -1,11 +1,36 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
-func CORS(allowedOrigin string) func(http.Handler) http.Handler {
+func CORS(allowedOriginsStr string) func(http.Handler) http.Handler {
+	allowedOrigins := strings.Split(allowedOriginsStr, ",")
+	for i := range allowedOrigins {
+		allowedOrigins[i] = strings.TrimSpace(allowedOrigins[i])
+	}
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+			origin := r.Header.Get("Origin")
+			allowedOrigin := ""
+
+			if allowedOriginsStr == "*" {
+				allowedOrigin = "*"
+			} else {
+				for _, o := range allowedOrigins {
+					if o == origin {
+						allowedOrigin = origin
+						break
+					}
+				}
+			}
+
+			if allowedOrigin != "" {
+				w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+			}
+
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
